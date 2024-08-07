613397e009b1598e985e17a2782222a5f06bbbc3
kyhol <Kyle.Hollett@keyin.com>
2024-08-07 11:43:30 -0230

    GenreYank added, with genretext etc.

---
+++ front-end/src/Components/GenreText/GenreText.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./GenreText.css";
+const GenreText = () => {
+  return (
+    <div className="wrapperOfGenreText">
+      <div className="genreTextContainer">
+        <p className="genreText">View by Genre</p>
+      </div>
+    </div>
+  );
+};
+
+export default GenreText;
