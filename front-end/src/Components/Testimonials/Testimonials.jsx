2d86ec29bd2cfdbab879fd434925da6aa2dc7b76
kyhol <Kyle.Hollett@keyin.com>
2024-08-07 10:00:46 -0230

    Spinning record img on the bot

--- front-end/src/Components/Testimonials/Testimonials.jsx
+++ front-end/src/Components/Testimonials/Testimonials.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import "./Testimonials.css";
+
+const testimonials = [
+  {
+    quote:
+      "My cat started giving me high-fives after I found this site. Coincidence? I think not.",
+    author: "William Butcher",
+  },
+  {
+    quote:
+      "I stayed up all night binge-shopping. My credit card company thinks my identity was stolen. Totally worth it!",
+    author: "Cherry Darling",
+  },
+  {
+    quote:
+      "This site is more addictive than my morning coffee. And I REALLY love coffee.",
+    author: "Richard Suka",
+  },
+];
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className="testimonials-container">
+      <h2>TESTIMONIALS</h2>
+      <div className="testimonials">
+        {testimonials.map((testimonial, index) => (
+          <div className="testimonial" key={index}>
+            <p className="quote">&ldquo;{testimonial.quote}&rdquo;</p>
+            <p className="author">&mdash; {testimonial.author}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
