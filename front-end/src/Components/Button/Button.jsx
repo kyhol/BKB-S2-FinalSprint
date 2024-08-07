3668a232204b0b218d4e72d40c0b3183ccee6954
Brian Janes <janes.brian@gmail.com>
2024-08-07 13:14:52 -0230

    product list modal good 2 go

--- front-end/src/Components/Button/Button.jsx
+++ front-end/src/Components/Button/Button.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaRegEye } from "react-icons/fa";
 import "./Button.css";
 
 const Button = ({ text, count, onClick, className, variant, isCheckout }) => {
@@ -8,7 +9,14 @@ const Button = ({ text, count, onClick, className, variant, isCheckout }) => {
 
   return (
     <button className={buttonClass} onClick={onClick}>
-      {text}
+      {variant === "quick-view" ? (
+        <>
+          <FaRegEye className="quick-view-icon" />
+          <span className="quick-view-text">{text}</span>
+        </>
+      ) : (
+        text
+      )}
       {count !== undefined && <span className="item-count">{count}</span>}
     </button>
   );
